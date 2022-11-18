9c59432b37e4c7151c1966a74e1f503e908565f7
alest1448 <118421044+alest1448@users.noreply.github.com>
2022-11-18 14:12:03 +0900

    Initial commit

---
+++ задание1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie
+{
+    internal class Program
+    {
+        public class Info
+        {
+            public static void name()
+            {
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Hello, {name}!");
+            }
+            static private int age;
+            static private int wee;
+            public static void age_()
+            {
+                Console.WriteLine("How you old?");
+                age = Convert.ToInt32(Console.ReadLine());
+                wee = age + 18;
+                if (age >= 18)
+                {
+                    Console.WriteLine($"You are old. We are {wee} for two.");
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"You are small. We are {wee} for two.");
+
+                }
+
+            }
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Info.name();
+            Info.age_();
+
+        }
+
+    }
+}
